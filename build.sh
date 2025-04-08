dbbeabad9ef110a631ff612f3b92852e7bcc833f
purhya <purhya@gmail.com>
2025-04-08 16:37:10 +0800

    Update build.sh to comment Linux builds

--- build.sh
+++ build.sh
@@ -102,11 +102,11 @@ for flavour in win32-ia32 win32-x64 win32-arm64v8; do
   fi
 done
 
-# Linux (x64, ARMv6, ARM64v8)
-for flavour in linux-x64 linuxmusl-x64 linux-armv6 linux-arm64v8 linuxmusl-arm64v8 linux-ppc64le linux-s390x; do
-  if [ $PLATFORM = "all" ] || [ $PLATFORM = $flavour ]; then
-    echo "Building $flavour..."
-    docker build -t vips-dev-$flavour platforms/$flavour
-    docker run --rm -e "VERSION_VIPS=${VERSION_VIPS}" -e VERSION_LATEST_REQUIRED -v $PWD:/packaging vips-dev-$flavour sh -c "/packaging/build/lin.sh $ADD_LIBDE265_OPTIONAL_DEPEDENCY"
-  fi
-done
+# # Linux (x64, ARMv6, ARM64v8)
+# for flavour in linux-x64 linuxmusl-x64 linux-armv6 linux-arm64v8 linuxmusl-arm64v8 linux-ppc64le linux-s390x; do
+#   if [ $PLATFORM = "all" ] || [ $PLATFORM = $flavour ]; then
+#     echo "Building $flavour..."
+#     docker build -t vips-dev-$flavour platforms/$flavour
+#     docker run --rm -e "VERSION_VIPS=${VERSION_VIPS}" -e VERSION_LATEST_REQUIRED -v $PWD:/packaging vips-dev-$flavour sh -c "/packaging/build/lin.sh $ADD_LIBDE265_OPTIONAL_DEPEDENCY"
+#   fi
+# done
